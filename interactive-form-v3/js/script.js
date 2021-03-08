2eb4ef8df1f9880b316a1bcbd99c8b625ab2379e
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-03-08 09:54:14 -0500

    Commit
    Commit

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -2,13 +2,22 @@
 const nameField = document.getElementById("name");
 nameField.focus();
 
+//This constant variable gets the shirt color, the color select
+const shirtColor = document.getElementById('color');
+const colorTheme = shirtColor.children;
 
+//This variable gets the shirt design and disables the color
+const designSelect = document.getElementById('design');
+shirtColor.disabled = true;
+
+const dataAttribute = document.getElementById('color').getAttribute('data-theme');
 
 //This const grabs the field and hides it when the page loads
 //This const grabs the select element
 const otherField = document.getElementById('other-job-role');
 const titleSelect = document.getElementById('title');
 otherField.hidden = true;
+
 
 //This handler is going to be a click handler for the select element
 titleSelect.addEventListener('change', (e) => {
@@ -18,23 +27,23 @@ titleSelect.addEventListener('change', (e) => {
     otherField.style.display = 'none';
     }
 })
-//This constant variable gets the shirt color
-const shirtColor = document.getElementById('color');
-//This variable gets the shirt design and disables the color
-const designSelect = document.getElementById('design');
-shirtColor.disabled = true;
-const currentTarget = event.target.value;
-const dataAttribute = document.getElementById('color').getAttribute('data-theme');
 
 //This handler enabled the shirt color when the select field is clicked
-designSelect.addEventListener('change', (event) => {
+designSelect.addEventListener('change', (e) => {
     shirtColor.disabled = false;
-        for (i = 0; i < shirtColor.children.length; i++ ){
-             currentTarget = event.target.value;
-             dataAttribute = document.getElementById('color').getAttribute('data-theme');
-             console.log(dataAttribute);
+    let dataAttribute = document.getElementById('color').getAttribute('data-theme');
+    let themeSelect = e.target.value;
+    colorTheme[0].textContent = 'Choose a color?';
+    color.value = 'Choose a color?';
 
-}})
+        for (i = 0; i < colorTheme.length; i++ ){
+            if ( themeSelect === dataAttribute ){
+                color.Theme[i].hidden = false;
+            } else if( themeSelect !== dataAttribute ){
+                colorTheme[i].hidden = true;
+            }
+        }
+    })
 
 // I was last watching the youtube video in the open tab about showing 
 // and hiding selections and input fields based on clicks

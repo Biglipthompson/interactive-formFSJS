78a3d69381c43edb8140100581b48edc5ad3c59c
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-03-15 00:05:26 -0400

    Commit
    Commit

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -2,22 +2,20 @@
 const nameField = document.getElementById("name");
 nameField.focus();
 
-
-
 //This constant variable gets the shirt color, the color select
-const shirtColor = document.getElementById('color');
-const colorTheme = shirtColor.children;
+const colorSelect = document.querySelector("#color");
+const colorTheme = colorSelect.children;
 
 //This variable gets the shirt design and disables the color
-const designSelect = document.getElementById('design');
-shirtColor.disabled = true;
-
+const designSelect = document.querySelector("#design");
+colorSelect.disabled = true;
 
 //This const grabs the field and hides it when the page loads
 //This const grabs the select element
 const otherField = document.getElementById('other-job-role');
 const titleSelect = document.getElementById('title');
 otherField.hidden = true;
+
 
 
 //This handler is going to be a click handler for the select element
@@ -29,31 +27,83 @@ titleSelect.addEventListener('change', (e) => {
     }
 })
 
-//This handler enabled the shirt color when the select field is clicked
+
 designSelect.addEventListener('change', (e) => {
-
-    shirtColor.disabled = false;
-
+    const jsPuns = document.querySelectorAll('[data-theme="js puns"]');
+    const heartJS = document.querySelectorAll('[data-theme="heart js"]');
+    const colorSelect = document.querySelector("#color");
     colorTheme[0].textContent = 'Choose a color?';
-    color.value = 'Choose a color?';
+    colorSelect.disabled = false;
 
-for (let i = 0; i < colorTheme; i++ ){
-    const themeSelect = e.target.value;
-    const dataAttribute = colorTheme[i].getAttribute('data-theme');
-
-    //if the event target equals the data theme
-
-    if ( themeSelect === dataAttribute ){
- 
-        color.Theme[i].hidden = false;
-        color.Theme[i].selected = true;
-
-        } else if ( themeSelect !== dataAttribute) {
-
-        colorTheme[i].hidden = true;
-        colorTheme[i].selected = false;
-
-        }
+    if (e.target.value === "js puns") {
+    for (let i = 0; i < jsPuns.length; i++) {
+        colorTheme[0].textContent = 'Choose a JS color?';
+        colorSelect.style.display = "block";
+        jsPuns[i].style.display = "block";
+        heartJS[i].style.display = "none";
+    }
+    } else if (e.target.value === "heart js") {
+    for (let i = 0; i < heartJS.length; i++) {
+        colorTheme[0].textContent = 'Choose a Heart color?';
+        colorSelect.style.display = "block";
+        heartJS[i].style.display = "block";
+        jsPuns[i].style.display = "none";
+    }
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     colorSelect.disabled = false;
+//     colorTheme[0].textContent = 'Choose a color?';
+//     color.value = 'Choose a color?';
+//     const jsPuns = document.querySelectorAll('[value="js puns"]');
+//     const heartJs = document.querySelectorAll('[value="heart js"]')
+//     const jsPunsColor = document.querySelectorAll('[data-theme="js puns"]');
+//     const heartJsColor = document.querySelectorAll('[data-theme="heart js"]')
+
+//     if(e.target.value === jsPuns){
+//         for (let i = 0; i < jsPunsColor.length; i++ ){
+//                 jsPunsColor[i].style.display = 'block';
+//                 heartJsColor[i].style.display = 'none';
+//                 colorSelect.style.display = 'none';
+//         }
+//     }
+    
+//     if(e.target.value === heartJs){
+//         for (let i = 0; i < heartJsColor.length; i++ ){
+//                 heartJsColor[i].style.display = 'block';
+//                 jsPunsColor[i].style.display = 'none';
+//                 colorSelect.style.display = 'none';
+//         }
+//     }
+// });
+
+
+    // const themeSelect = e.target.value;
+    // const dataAttribute = getAttribute('data-theme').colorTheme[i];
+    // if ( themeSelect === dataAttribute ){
+    //     color.Theme[i].hidden = false;
+    //     color.Theme[i].selected = true;
+    //     } else if ( themeSelect !== dataAttribute) {
+    //     colorTheme[i].hidden = true;
+    //     colorTheme[i].selected = false;
+    //     }
+
+
 

cb30fdeba3f4ba53ad7c766208312bcdbee27d66
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-04-05 21:59:52 -0400

    Commit

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -240,15 +240,24 @@ if (!cvvValidator()) {
 }}});
 
 
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-// accessibility section 
+// const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+// // accessibility section 
+// for (let i = 0; i < checkboxes.length; i++) {
+//     checkboxes[i].addEventListener('focus', () => { 
+//         checkboxes[i].parentElement.classList.add('focus');
+//     });
+//     checkboxes[i].addEventListener('blur', () => { 
+//         checkboxes[i].parentElement.classList.remove('focus');
+//     });
+// }
+
+const checkboxes = document.querySelectorAll('input[type ="checkbox"]');
 for (let i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener('focus', () => { 
-        checkboxes[i].parentElement.classList.add('focus');
+    checkboxes[i].addEventListener('focus', () => {
+    checkboxes[i].parentElement.classList.add('focus');
     });
-    checkboxes[i].addEventListener('blur', () => { 
-        checkboxes[i].parentElement.classList.remove('focus');
+checkboxes[i].addEventListener('blur', () => {
+    checkboxes[i].parentElement.classList.remove('focus');
     });
 }
-
 
